a48c655b913e4652e3e4287dd8e9b5753acefd72
Nikhil Singh <nikhilsinghcr007@gmail.com>
2024-05-28 16:11:57 +0530

    URL Updated

--- book/urls.py
+++ book/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('api/register/', RegisterView.as_view(), name='register'),
     path('api/', include('api.urls')),
 ]
